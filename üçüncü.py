5f9c22179dc80532e388762d7fa67c8c315d84ff
selinaarow <101040097+selinaarow@users.noreply.github.com>
2022-03-06 12:01:39 +0300

    Add files via upload

---
+++ üçüncü.py
@@ -0,0 +1,14 @@
+gunler=["pazartesi", "salı", "çarşamba"]
+gunler.pop(1)
+print(gunler)
+
+gunler=["pazartesi", "salı", "çarşamba"]
+gunler.pop()
+
+print(gunler)
+
+gunler.pop(0)
+print(gunler)
+
+
+
