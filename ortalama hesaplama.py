4ca697eda166a34864f582f459d4555c88c7a9d3
selinaarow <101040097+selinaarow@users.noreply.github.com>
2022-03-06 12:02:01 +0300

    Add files via upload

---
+++ ortalama hesaplama.py
@@ -0,0 +1,10 @@
+ortalama=int(input("bir not giriniz:"))
+
+if takdir>= 85:
+     print("tebrikler takdir alabiliyorsun")
+
+elif teşekkür>=70:
+     print("tebrikler teşekkür alabiliyorsun")
+
+else:
+   print("hiçbir belge alamıyorsun")
